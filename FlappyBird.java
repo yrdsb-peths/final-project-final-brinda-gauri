be32e246369682b7769f5bcb8993bb484524ad43
gauri-n08 <gaurisknair08@gmail.com>
2025-05-20 08:40:47 -0400

    Add & Resize Flappy Bird

---
+++ FlappyBird.java
@@ -0,0 +1,22 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class FlappyBird here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class FlappyBird extends Actor
+{
+    public FlappyBird()
+    {
+        GreenfootImage flappy = new GreenfootImage("FlappyBird.png");
+        flappy.scale(100,100);
+        setImage(flappy);
+    }
+    
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
--- MyWorld.java
+++ MyWorld.java
@@ -3,5 +3,10 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        
+        FlappyBird flappy = new FlappyBird();
+        addObject(flappy, 300, 300);
     }
+    
+    
 }
