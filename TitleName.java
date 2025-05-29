187501f5585096bfde457ba07c642047b565ae4f
gauri-n08 <gaurisknair08@gmail.com>
2025-05-29 08:38:20 -0400

    Change Title Image

--- TitleName.java
+++ TitleName.java
@@ -4,8 +4,8 @@ public class TitleName extends Actor
 {
     public TitleName()
     {
-        GreenfootImage title = new GreenfootImage("TitleName.png");
-        title.scale(500,200);
+        GreenfootImage title = new GreenfootImage("Flappy_Logo.png");
+        title.scale(550,200);
         setImage(title);
     }
 }
