26a569937055249dc5108db7e005121876dafe00
gauri-n08 <gaurisknair08@gmail.com>
2025-05-29 09:04:15 -0400

    Resize TItle

--- TitleName.java
+++ TitleName.java
@@ -5,7 +5,7 @@ public class TitleName extends Actor
     public TitleName()
     {
         GreenfootImage title = new GreenfootImage("Flappy_Logo.png");
-        title.scale(550,200);
+        title.scale(650,200);
         setImage(title);
     }
 }
