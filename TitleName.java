273cbb277f63a5be51acf01059c2518a05ac8cf0
brinda-n08 <349035105@gapps.yrdsb.ca>
2025-06-06 09:07:28 -0400

    Added comments to TitleName

--- TitleName.java
+++ TitleName.java
@@ -2,6 +2,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public class TitleName extends Actor
 {
+    //This method sets the title image
     public TitleName()
     {
         GreenfootImage title = new GreenfootImage("Flappy_Logo.png");
