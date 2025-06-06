9ffb4ffb9378a6c75ed207de7fd86771248fa7f5
brinda-n08 <349035105@gapps.yrdsb.ca>
2025-06-06 08:44:07 -0400

    Added comments to TitleScreen

--- TitleScreen.java
+++ TitleScreen.java
@@ -2,6 +2,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public class TitleScreen extends World
 {    
+    //This method creates a title screen by adding the background images and several labels.
     public TitleScreen()
     {    
         super(800, 450, 1); 
@@ -11,6 +12,7 @@ public class TitleScreen extends World
         prepare();
     }
     
+    //This method switches to the game world when the space key is pressed
     public void act()
     {
         if(Greenfoot.isKeyDown("space"))
@@ -20,6 +22,10 @@ public class TitleScreen extends World
         }
     }
     
+    /**
+     * This method adds labels and objects showing the title name, how to play,
+     * and 3 animated flappys
+     */
     private void prepare()
     {
         Label spaceLabel = new Label("Press <space> to Start & Play", 30);
